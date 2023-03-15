1f377fc1e5acb3e4e1e7e5f55bed4412a894b860
Detkov Vasiliy <fgh.detkov@gmail.com>
2023-03-15 14:03:34 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {useState} from 'react'
 import './App.css'
 import image from './image.jpg'
 import image2 from './image2.jpg'
+import image3 from './image3.jpg'
 import {Slider} from "./components/slider/slider";
 
 
@@ -31,6 +32,13 @@ function App() {
                     jobPosition: 'Футболист Дортмундской Боруссии',
                     text: 'Моя жизнь изменилась, я проснулся',
                     image: image2,
+                },
+                {
+                    id: 3,
+                    name: "Томас Тухель",
+                    jobPosition: 'Домохозяин',
+                    text: 'Жаль, что не вступил в Local English Speaking Club пока работал тренером в Челси',
+                    image: image3,
                 }
             ]}/>
         </div>
