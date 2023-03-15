66e61f31295d0e6addda18a0e4e9f7f15628f71e
Detkov Vasiliy <fgh.detkov@gmail.com>
2023-03-15 23:11:49 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,11 @@
-import {useState} from 'react'
 import './App.css'
-import image from './image.jpg'
-import image2 from './image2.jpg'
-import image3 from './image3.jpg'
 import {Slider} from "./components/slider/slider";
-
+import {Items} from './utils'
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
-        <div className="App"
-             style={{
-                 display: 'flex',
-                 justifyContent: 'center',
-                 alignItems: 'center',
-                 height: '100vh',
-             }}
-        >
-            <Slider reviews={[
-                {
-                    id: 1,
-                    name: "Виктор Комаров",
-                    jobPosition: 'Продавец Wildberries',
-                    text: 'Просто потрясающе, я заговорил на английском так, как сама не предполагал',
-                    image: image,
-                },
-                {
-                    id: 2,
-                    name: "Матс Хуммельс",
-                    jobPosition: 'Футболист Дортмундской Боруссии',
-                    text: 'Моя жизнь изменилась, я проснулся',
-                    image: image2,
-                },
-                {
-                    id: 3,
-                    name: "Томас Тухель",
-                    jobPosition: 'Домохозяин',
-                    text: 'Жаль, что не вступил в Local English Speaking Club пока работал тренером в Челси',
-                    image: image3,
-                }
-            ]}/>
+        <div className="App">
+            <Slider reviews={Items}/>
         </div>
     )
 }
---
+++ src/utils.tsx
@@ -0,0 +1,27 @@
+import image from "./image.jpg";
+import image2 from "./image2.jpg";
+import image3 from "./image3.jpg";
+
+export const Items = [
+    {
+        id: 1,
+        name: "Виктор Комаров",
+        jobPosition: 'Продавец Wildberries',
+        text: 'Просто потрясающе, я заговорил на английском так, как сам не предполагал',
+        image: image,
+    },
+    {
+        id: 2,
+        name: "Матс Хуммельс",
+        jobPosition: 'Футболист Дортмундской Боруссии',
+        text: 'Моя жизнь изменилась, я проснулся',
+        image: image2,
+    },
+    {
+        id: 3,
+        name: "Томас Тухель",
+        jobPosition: 'Домохозяин',
+        text: 'Жаль, что не вступил в Local English Speaking Club пока работал тренером в Челси',
+        image: image3,
+    }
+]
